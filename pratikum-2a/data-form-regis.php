9ca3b2ea36e274c4e1c5cc1b8b487cd25358ce07
adibacantik123 <adibatulhasanah37@gmail.com>
2025-03-06 08:55:22 +0700

    praktikum02

---
+++ pratikum-2a/data-form-regis.php
@@ -0,0 +1,19 @@
+<?php
+
+$ar_prodi = [
+    'SI' => 'Sistem Informasi',
+    'TI' => 'Teknik Informatika',
+    'BD' => 'Bisnis Digital'
+];
+
+$ar_skill = [
+    'HTML' => 10,
+    'CSS' => 10,
+    'Javacript' => 20,
+    'RWD Bootstrap' => 20,
+    'PHP' => 30,
+    'Phyton' => 30,
+    'Java' => 50
+];
+
+$ar_Domisili = ["Jakarta", "Depok", "Bogor", "Tanggerang", "Bekasi", "Lainnya"];
